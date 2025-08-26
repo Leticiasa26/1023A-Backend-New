4e6da583c653a7fee08aeba9156fa77d015741b9
leticiasa26 <leticia.alves3@estudante.ifms.edu.br>
2025-08-26 10:42:58 -0400

    Comite da aula 26/08

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,69 @@
 import 'dotenv/config'
-import express from 'express';
+import express, { Request, Response } from 'express';
+import mysql from 'mysql2/promise';
 
 console.log ( process.env.DBUSER );
 const app = express ();
 
-app.get ( '/', ( req, res ) => {
+// ! singifica que é a negação da variável 
+
+app.get ( '/', async ( req:Request, res:Response ) => {
+
+    if ( ! process.env.DBHOST ) {
+        res.status ( 500 ) . send ( "Variável de ambiente DBHOST não está difinida" )
+        return;
+    }    
+
+    if ( ! process.env.DBUSER ) {
+        res.status ( 500 ) .send ( "Variável de ambiente DBUSER não está definida" )
+        return;
+    }
+
+    if ( process.env.DBPASSWORD == undefined ) {
+        res.status ( 500 ) .send ( "Variável de ambiente DBPASSWORD não está definida" )
+        return;
+    }
+
+    if ( ! process.env.DBNAME ) {
+        res.status ( 500 ) .send ( "Variável de ambiente DBNAME não está definida" )
+        return;
+    }
+
+    if ( ! process.env.DBPORT ) {
+        res.status ( 500 ) .send ( "Variável de ambiente DBPORT não está definida" )
+        return;
+    }
+
+    try {
+
+    const connection =  await mysql.createConnection ( {
+    
+        host: process.env.DBHOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBNAME,
+        port: Number ( process.env.DBPORT )
+    
+    } )
+
+    res.send ( "Conectado ao bando de dados com sucesso!" )
+
+    await connection.end ();
+
+}
+
+catch ( error ) {
+
+    res.status ( 500 ) .send ( " Erro ao conectar ao banco de dados: " + error );
+
+}
+
     res.send ( process.env.DBUSER );
+
 });
 
 app.listen ( 8000, () => {
-    console.log ( 'O servidor está rodando na porta 8000');
+
+    console.log ( 'O servidor está rodando na porta 8000' );
+    
 });
